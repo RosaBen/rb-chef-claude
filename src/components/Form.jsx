94c32c7b4980d5b4ac023c85aeb0b53ba051ad58
RosaBen <benchabane.rosa@outlook.com>
2026-05-15 01:31:02 +0200

    added submit listener, removed form components, list is updated with new ingredient

--- src/components/Form.jsx
+++
@@ -1,8 +0,0 @@
-export default function Form(){
-  return(
-    <form>
-      <input type="text" placeholder="e.g. oregano" aria-label="Add ingredient"/>
-      <button type="submit">+ Add ingredient</button>
-    </form>
-  )
-}
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,16 +1,26 @@
-import Form from "./Form";
+
 
 export default function Main(){
   const ingredients = ["Chicken", "Oregano", "Tomatoes"]
 
   const listIngredients = ingredients.map(ingredient => {
     return (
-      <li>{ingredient}</li>
+      <li key={ingredient}>{ingredient}</li>
     )
   })
+
+  function handleClick (e){
+      e.preventDefault()
+    const formData = new FormData(e.currentTarget)
+    const newIngredient = formData.get("ingredient")
+    ingredients.push(newIngredient)
+  }
   return(
     <main>
-      <Form />
+    <form onSubmit={handleClick}>
+      <input type="text" placeholder="e.g. oregano" aria-label="Add ingredient" name="ingredient"/>
+      <button type="submit">+ Add ingredient</button>
+    </form>
       <div className="listIngredients">
               <h1>Ingredients on hand:</h1>
       <ul>
